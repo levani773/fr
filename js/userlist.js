11723a59472fb74775698cdd89cdf4fda0f13b87
levani773 <113704626+levani773@users.noreply.github.com>
2022-10-15 19:00:25 +0200

    new

---
+++ js/userlist.js
@@ -0,0 +1,64 @@
+let mainWraper = document.getElementById('post-block');
+let overlay = document.getElementById('overlay');
+let close = document.getElementById('close');
+
+//http://jsonplaceholder.typicode.com/posts
+
+function ajax(url, callback) {
+    let requist = new XMLHttprequest();
+    requist.open('GET', url);
+
+    requist.addEventListener('load', function() {
+        let data = JSON.parse(requist.responseText);
+        callback(data);  
+       
+    });
+
+    requist.send();
+}
+
+ajax('http://jsonplaceholder.typicode.com/posts', function(data) {
+    printdata(data);
+});
+
+function printdata(data) {
+    data.forEach(element => {
+        createPost(element);
+    });
+}
+
+function createPost(item) {
+    let divWraper = document.createElement('div'); //შევქმენი დივი
+    divWraper.classList.add('posts');
+    divWraper.setAttribute('data-id', item.id);
+
+    let h1 = document.createElement('h1'); //შევქმენი 
+    h1.innerText = item.id;
+
+    let text = document.createElement('div'); //შევქმენი მეორე დივი
+    text.classList.add('title');
+    text.innerText = item.title;
+    
+    divWraper.appendChild(h1);  //ჩავსვი დივში
+    divWraper.appendChild(text); //ჩავსვი დივში
+
+    divWraper.addEventListener('click', function(event) {
+        let id = event.target.getAttribute('data-id');
+        openoverlay(id);
+    }); 
+
+    mainWraper.appendChild(divWraper);
+}
+
+function openoverlay(id) {
+    overlay.classList.add('active');
+    let url = 'http://jsonplaceholder.typicode.com/posts/$[id]';
+    ajax(url, function(data) {
+        console.log(data);
+    }) 
+    console.log(id);
+}
+close.addEventListener('click', function() {
+    overlay.classList.remove('active');
+});
+
